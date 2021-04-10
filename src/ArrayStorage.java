fd6046891b31921845655a41bb8f76b83974d2ad
Anton23rus <anton23rus@gmail.com>
2021-04-11 00:37:47 +0300

    home work 2 first try

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,49 +8,77 @@ public class ArrayStorage {
     int storageSize = 0;
 
     void clear() {
-        for (int i = 0; i < storageSize; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, 0, storageSize - 1, null);
         storageSize = 0;
     }
 
-    void save(Resume r) {
-        for (int i = storageSize; i >= 0; i--) {
-            if (i == 0) {
-                storage[i] = r;
-                storageSize++;
-            } else {
-                storage[i] = r;
-                storageSize++;
-                break;
+
+    int elementExists(String guid) {
+        int isExists = -1;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(guid)) {
+                isExists = i;
             }
+        }
+        return isExists;
+    }
+
+    void update(Resume r) {
+        int index = elementExists(r.uuid);
+        if (index >= 0) {
+            storage[index] = r;
+        } else {
+            System.out.println("Невозможно обновить! Данного резюме нет в базе!");
+        }
+    }
+
+
+    void save(Resume r) {
+        if (storageSize < storage.length) {
+            if (elementExists(r.uuid) < 0) {
+                for (int i = storageSize; i >= 0; i--) {
+                    if (i == 0) {
+                        storage[i] = r;
+                        storageSize++;
+                    } else {
+                        storage[i] = r;
+                        storageSize++;
+                        break;
+                    }
+                }
+            } else {
+                System.out.println("Данное резюме уже содержится в базе! Запись не требуется.");
+            }
+        } else {
+            System.out.println("База резюме переполнена!");
         }
     }
 
     Resume get(String uuid) {
-        for (Resume r : storage) {
-            if (r.uuid.equals(uuid)) {
-                return r;
-            }
+        int index = elementExists(uuid);
+        if (index >= 0) {
+            return storage[index];
+        } else {
+            System.out.println("Резюме с указанным uuid нет в базе!");
+            return null;
         }
-        return null;
+
+
     }
 
     void delete(String uuid) {
-        boolean done = false;
-        for (int i = 0; i < storageSize; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                storage[i] = null;
-                done = true;
-                for (int x = i; x < storageSize; x++) {
-                    storage[x] = storage[x + 1];
-                    storage[x + 1] = null;
-                    break;
-                }
-                storageSize--;
+        int index = elementExists(uuid);
+        if (index >= 0) {
+            storage[index] = null;
+            for (int x = index; x < storageSize; x++) {
+                storage[x] = storage[x + 1];
+                storage[x + 1] = null;
             }
-            if (done) break;
+            storageSize--;
+        } else {
+            System.out.println("Резюме с указанным uuid нет в базе!");
         }
+
     }
 
     /**
