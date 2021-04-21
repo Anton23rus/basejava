735f01b1b79d3b163b97f397e92fec95928032e4
Anton23rus <anton23rus@gmail.com>
2021-04-21 23:50:56 +0300

    Домашка 3 попытка 2

--- src/com/baseJava/webapp/storage/SortedArrayStorage.java
+++ src/com/baseJava/webapp/storage/SortedArrayStorage.java
@@ -6,52 +6,23 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-
-    @Override
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index < 0) {
-            System.out.println("Resume " + r.getUuid() + " not exist");
-        } else {
-            storage[index] = r;
-        }
-    }
-
-    @Override
-    public void save(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index >= 0) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            storage[countIndex(index)] = r;
-            size++;
-        }
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index < 0) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            for (int i = index; i < size; i++) {
-                if (i != (size - 1)) {
-                    storage[i] = storage[i + 1];
-                } else {
-                    storage[i] = null;
-                }
-            }
-            size--;
-        }
-    }
-
     @Override
     protected int getIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+
+    @Override
+    protected void createElement(Resume resume, int index) {
+        storage[countIndex(index)] = resume;
+    }
+
+    @Override
+    protected void moveElements(int index) {
+        for (int i = index; i < size - 1; i++) {
+            storage[i] = storage[i + 1];
+        }
     }
 
     private int countIndex(int index) {
